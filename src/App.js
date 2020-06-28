8015d448861a3bf6f48bf4c222e4181e0545a48d
Sergei <SPetrenko17@yandex.ru>
2020-06-28 23:37:17 +0300

    auth with router

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import {Provider} from "react-redux";
 import './index.scss';
 import Landing from "./moduels/landing";
 import Authorization from "./moduels/Authorization/Authorization";
-
+import { Switch, Route, Redirect } from 'react-router';
 
 function App() {
-
   const store = createStore(reducer);
 
   return (
     <Provider store={store}>
       <div className="App">
+          <main className='main'>
+              <Switch>
+                  <Route path='/landing' component={Landing} />
+                  <Route exact path='/login' component={() => <Authorization type={'login'} />} />
+                  <Route exact path='/reg' component={() => <Authorization type={'reg'} />} />
+                  <Redirect to='/'/>
+              </Switch>
+          </main>
         {/*<Landing/>*/}
-        <Authorization type="reg"/>
+        {/*<Authorization type="reg"/>*/}
       </div>
     </Provider>
   );
