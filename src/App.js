4b0b70d1539089229d83a933868efadd39f25471
tim-po <Q3xzc6vv>
2020-06-28 17:48:36 +0300

    intermidiate

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        {/*<Landing/>*/}
-        <Authorization type="reg"/>
+        <Landing/>
+        {/*<Authorization type="reg"/>*/}
       </div>
     </Provider>
   );
